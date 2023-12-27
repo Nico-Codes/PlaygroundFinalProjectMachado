093ddc27cb1aa72da0ab3b3a9179a372de8108c8
Nico-Codes <nicolas5905code@gmail.com>
2023-12-27 01:05:10 -0300

    Final 3.1

--- ProyectoCoderMachado/ProyectoCoder/settings.py
+++ ProyectoCoderMachado/ProyectoCoder/settings.py
@@ -83,11 +83,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
-     'test': {
+    },
+    'test': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / "db_test.sqlite3",
-    }
+    },  # Asegúrate de tener una coma aquí
 }
 
 
